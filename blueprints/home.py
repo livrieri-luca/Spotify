783fb272b4a96d5d26f57b575da3511790c8cf93
livrieri-luca lucon <livrieri.luca@iisgalvanimi.edu.it>
2025-02-24 14:01:20 +0000

    controllato

--- blueprints/home.py
+++ blueprints/home.py
@@ -18,8 +18,6 @@ def homepage():
 
     return render_template('home.html', user_info=user_info, playlists=playlists)
 
-
-
 @home_bp.route('/playlist_tracks/<playlist_id>')
 def playlist_tracks(playlist_id):
     token_info = session.get('token_info', None)
@@ -30,3 +28,4 @@ def playlist_tracks(playlist_id):
     tracks = sp.playlist_tracks(playlist_id)['items']
 
     return render_template('playlist_tracks.html', tracks=tracks)
+
