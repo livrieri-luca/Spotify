b761dfab4e0c9b059b9c55b86624a9f81bcc36ab
livrieri-luca lucon <livrieri.luca@iisgalvanimi.edu.it>
2025-02-24 17:38:28 +0000

    parte 1 della funzionalita' a piacere fatta

--- blueprints/home.py
+++ blueprints/home.py
@@ -4,6 +4,7 @@ from services.spotify_oauth import get_spotify_object
 
 home_bp = Blueprint('home', __name__)
 
+# Route per la homepage con le playlist
 @home_bp.route('/home')
 def homepage():
     token_info = session.get('token_info', None)
@@ -18,6 +19,7 @@ def homepage():
 
     return render_template('home.html', user_info=user_info, playlists=playlists)
 
+# Route per visualizzare i brani della playlist
 @home_bp.route('/playlist_tracks/<playlist_id>')
 def playlist_tracks(playlist_id):
     token_info = session.get('token_info', None)
@@ -27,5 +29,24 @@ def playlist_tracks(playlist_id):
     sp = spotipy.Spotify(auth=token_info['access_token'])
     tracks = sp.playlist_tracks(playlist_id)['items']
 
-    return render_template('playlist_tracks.html', tracks=tracks)
+    return render_template('playlist_tracks.html', tracks=tracks, playlist_id=playlist_id)
 
+# Nuova route per visualizzare i dettagli di un brano (in una pagina separata)
+@home_bp.route('/track_details/<track_id>')
+def track_details(track_id):
+    token_info = session.get('token_info', None)
+    if not token_info:
+        return redirect(url_for('auth.login'))  
+
+    sp = spotipy.Spotify(auth=token_info['access_token'])
+    
+    # Recupera i dettagli completi del brano tramite l'ID
+    track = sp.track(track_id)
+    artist_id = track['artists'][0]['id']
+    
+    # Recupera informazioni sull'artista (incluso il genere)
+    artist_details = sp.artist(artist_id)
+    genre = artist_details.get('genres', ['Genere sconosciuto'])[0]  # Genere principale dell'artista
+    
+    # Passa i dettagli alla pagina di visualizzazione
+    return render_template('track_details.html', track=track, genre=genre)
