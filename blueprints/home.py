8d6b0f60708612ccf65250c83cad4c16b062fc00
livrieri-luca lucon <livrieri.luca@iisgalvanimi.edu.it>
2025-03-10 15:22:17 +0000

    Riordinamento api

--- blueprints/home.py
+++ blueprints/home.py
@@ -1,6 +1,5 @@
 from flask import Blueprint, redirect, request, url_for, session, render_template
-import spotipy
-from services.spotify_oauth import get_spotify_object
+from services.spotify_oauth import get_spotify_object, get_user_info, get_user_playlists, get_playlist_tracks, get_track_details
 
 home_bp = Blueprint('home', __name__)
 
@@ -9,9 +8,8 @@ def homepage():
     token_info = session.get('token_info', None)
 
     if token_info:
-        sp = get_spotify_object(token_info)
-        user_info = sp.current_user()
-        playlists = sp.current_user_playlists()['items']
+        user_info = get_user_info(token_info)
+        playlists = get_user_playlists(token_info)
     else:
         user_info = None
         playlists = None  # Nessuna playlist se non autenticato
@@ -23,10 +21,9 @@ def homepage():
 def playlist_tracks(playlist_id):
     token_info = session.get('token_info', None)
     if not token_info:
-        return redirect(url_for('auth.login'))  
+        return redirect(url_for('auth.login'))
 
-    sp = spotipy.Spotify(auth=token_info['access_token'])
-    tracks = sp.playlist_tracks(playlist_id)['items']
+    tracks = get_playlist_tracks(token_info, playlist_id)
 
     return render_template('playlist_tracks.html', tracks=tracks, playlist_id=playlist_id)
 
@@ -35,24 +32,11 @@ def playlist_tracks(playlist_id):
 def track_details(track_id):
     token_info = session.get('token_info', None)
     if not token_info:
-        return redirect(url_for('auth.login'))  
+        return redirect(url_for('auth.login'))
 
-    sp = spotipy.Spotify(auth=token_info['access_token'])
-    
-    track = sp.track(track_id)
-    artist_id = track['artists'][0]['id']
-    
-    artist_details = sp.artist(artist_id)
-    
-    # Ottieni la lista dei generi in modo sicuro
-    genre = artist_details.get('genres', [])
-    
-    # Se la lista è vuota, imposta il genere di default
-    genre = genre[0] if genre else 'Genere sconosciuto'
+    track, genre = get_track_details(token_info, track_id)
 
     # Ottieni l'ID della playlist dai parametri della query
     playlist_id = request.args.get('playlist_id')
 
     return render_template('track_details.html', track=track, genre=genre, playlist_id=playlist_id)
-
-
--- services/spotify_oauth.py
+++ services/spotify_oauth.py
@@ -4,7 +4,7 @@ from spotipy.oauth2 import SpotifyOAuth, SpotifyClientCredentials
 # Configurazione credenziali Spotify
 SPOTIFY_CLIENT_ID = "200875a1e6d941bebe8d3ab86bd8dadf"
 SPOTIFY_CLIENT_SECRET = "c35e9f794b0e44baaf935f5e8638b320"
-SPOTIFY_REDIRECT_URI = "https://5000-livrieriluca-spotify-ls4mh88j8rr.ws-eu118.gitpod.io/callback"
+SPOTIFY_REDIRECT_URI = "https://5000-livrieriluca-spotify-88u1ji5dncl.ws-eu118.gitpod.io/callback"
 SPOTIFY_SCOPE = "user-read-private user-read-email playlist-read-private"
 
 # Autenticazione con OAuth per utenti loggati
@@ -27,3 +27,28 @@ def get_spotify_object(token_info=None):
     if token_info:
         return spotipy.Spotify(auth=token_info['access_token'])
     return sp_public  # Accesso limitato per utenti non autenticati
+
+# Funzione per ottenere l'informazione dell'utente attualmente autenticato
+def get_user_info(token_info):
+    sp = get_spotify_object(token_info)
+    return sp.current_user()
+
+# Funzione per ottenere le playlist dell'utente
+def get_user_playlists(token_info):
+    sp = get_spotify_object(token_info)
+    return sp.current_user_playlists()['items']
+
+# Funzione per ottenere i brani di una playlist
+def get_playlist_tracks(token_info, playlist_id):
+    sp = get_spotify_object(token_info)
+    return sp.playlist_tracks(playlist_id)['items']
+
+# Funzione per ottenere i dettagli di un brano
+def get_track_details(token_info, track_id):
+    sp = get_spotify_object(token_info)
+    track = sp.track(track_id)
+    artist_id = track['artists'][0]['id']
+    artist_details = sp.artist(artist_id)
+    genre = artist_details.get('genres', [])
+    genre = genre[0] if genre else 'Genere sconosciuto'
+    return track, genre
