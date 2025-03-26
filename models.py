8d70fa1f16f7231a1ca709355f368405561f37ab
livrieri-luca lucon <livrieri.luca@iisgalvanimi.edu.it>
2025-03-26 14:48:22 +0000

    ricontrollato

--- models.py
+++ models.py
@@ -13,8 +13,3 @@ class ListaPlaylist(db.Model):
     utente = db.Column(db.String(80), nullable=False)
     nome = db.Column(db.String(80), nullable=False)
     elemento = db.Column(db.String(100), nullable=False)
-class Elemento(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    nome = db.Column(db.String(100), nullable=False)
-    tipo = db.Column(db.String(50), nullable=False)  # ad esempio "canzone", "video", ecc.
-    playlist_id = db.Column(db.Integer, db.ForeignKey('lista_playlist.id'), nullable=False)
